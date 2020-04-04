1a5698ad1df4c5d4a8f209b1ae7b8e9f896d0283
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-04 19:38:35 +0300

    Updates

---
+++ precache-manifest.c50a0d2e4b061645ec010d7fa33cf8f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f20ec4f0342506391b33c97a58782c6",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "f1f6f0cbd56c3fcbd65d",
+    "url": "/phonebook/static/css/main.2de32599.chunk.css"
+  },
+  {
+    "revision": "7e53469d9daf6d75e15f",
+    "url": "/phonebook/static/js/2.2abbc96a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phonebook/static/js/2.2abbc96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f6f0cbd56c3fcbd65d",
+    "url": "/phonebook/static/js/main.f75d5eb3.chunk.js"
+  },
+  {
+    "revision": "9ec571ae1d9300ef205a",
+    "url": "/phonebook/static/js/runtime-main.de48658b.js"
+  },
+  {
+    "revision": "f250dc652592cd94dafad7307c1e2723",
+    "url": "/phonebook/static/media/phone-container-grey.f250dc65.jpg"
+  },
+  {
+    "revision": "3b2dbc4d68847bffecd1be245635eb2e",
+    "url": "/phonebook/static/media/phone-container.3b2dbc4d.jpg"
+  }
+]);
